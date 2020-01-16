7812fb8eaa9aeb4541c95074434ce6c1793b4d24
eckysaroyd <enyato98@gmail.com>
2020-01-16 14:44:53 +0530

    new switch statement

---
+++ python project/three_week.py
@@ -0,0 +1,18 @@
+#number of the weeks
+day=input("Enter from  day 1 to 7 by using number  ")
+if (day==1):
+    print("monday")
+elif(day==2):
+    print("tuesday")
+elif(day==3):
+    print("wenesday")
+elif(day==4):
+    print("thurday")
+elif(day==5):
+    print("friday")
+elif(day==6):
+    print("tuesday")
+elif(day==7):
+    print("tuesday")
+else:
+    print("not a day")
