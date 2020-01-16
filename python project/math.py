0163545ad519723a81f5ac309e973203ad0d50f4
eckysaroyd <enyato98@gmail.com>
2020-01-16 14:57:00 +0530

    new math calculation

---
+++ python project/math.py
@@ -0,0 +1,7 @@
+import math
+m =int (input("enter any number"))
+print ("factorial :-",math.factorial(m))
+print ("squareroot:-",math.sqrt (m))
+print ("floor:-",math.floor(m))
+print ("celing:-",math.ceil(m))
+print("log:-",math.log(m))
