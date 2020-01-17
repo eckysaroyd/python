e2889677cba6d52cef8a83412dd7de9b8ad2f30c
eckysaroyd <enyato98@gmail.com>
2020-01-17 14:37:47 +0530

    arithmetics

---
+++ python project/arithmeticsoperation.py
@@ -0,0 +1,30 @@
+import math 
+
+a = -10
+b = 5.5
+c = 15
+d = 5
+
+
+print ("The ceil of 2.3 is : ") 
+print (math.ceil(a)) 
+
+
+print ("The floor of 2.3 is : ") 
+print (math.floor(a)) 
+
+ 
+print ("The absolute value of -10 is : ") 
+print (math.fabs(a)) 
+
+print ("The factorial of 5 is : ") 
+print (math.factorial(b)) 
+
+
+ 
+print ("The copysigned value of -10 and 5.5 is : ") 
+print (math.copysign(5.5, -10)) 
+
+
+print ("The gcd of 5 and 15 is : ") 
+print (math.gcd(5,15)) 
