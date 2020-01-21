8cccb6a6badcc3a39533ec391d5054f08a839629
eckysaroyd <enyato98@gmail.com>
2020-01-21 15:23:15 +0530

    new repo

--- python project/pythagorus2.py
+++
@@ -1,9 +0,0 @@
-import math
-m =int (input("enter any number"))
-n =int (input("enter any number"))
-c = a**2 + b**2
-print("after a square + b square  is ",c)
-
-sum = sqrt(num)
-
-print("Your Result after pythagorus is =" , sum , "\n")
